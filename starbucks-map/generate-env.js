337c6e3c7c62d83876f33bb4479bdcce7164754d
Conanlag <alberto.mejia4395@alumnos.udg.mx>
2026-07-21 15:51:04 -0600

    Ignore generated index.html

--- starbucks-map/generate-env.js
+++ starbucks-map/generate-env.js
@@ -1,6 +1,7 @@
 const fs = require("fs");
 const path = require("path");
 
+// Generar environment
 const envContent = `
 export const environment = {
   production: false,
@@ -16,20 +17,17 @@ fs.writeFileSync(
   envContent
 );
 
-// index.html
-const template = fs.readFileSync(
-  path.join(__dirname, "src/index.html"),
-  "utf8"
-);
+// Generar index.html
+const templatePath = path.join(__dirname, "src/index.html.template");
+const outputPath = path.join(__dirname, "src/index.html");
 
-const index = template.replace(
+const template = fs.readFileSync(templatePath, "utf8");
+
+const output = template.replace(
   /\$\{GOOGLE_MAPS_API_KEY\}/g,
   process.env.GOOGLE_MAPS_API_KEY
 );
 
-fs.writeFileSync(
-  path.join(__dirname, "src/index.html"),
-  index
-);
+fs.writeFileSync(outputPath, output);
 
 console.log("Environment and index generated.");
